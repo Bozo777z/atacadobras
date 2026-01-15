12415beb659a73d3783023c9b3cc7cf78abaca79
Bozo777z <angelamariacash@gmail.com>
2026-01-14 22:46:23 -0300

    Add Next.js configuration for i18n support

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR'
+  }
+};
+module.exports = nextConfig;
